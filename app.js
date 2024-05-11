6a23634de39179bdc0f9481978821cef584c058e
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-11 17:11:38 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+// localStorage.setItem('key', 'value');
+// localStorage.getItem('key'); // returns 'value'
+// localStorage.removeItem('key');
+
+// // Path: app.js
+// // Save data to localStorage
+
+// localStorage.setItem('name', 'John Doe');
+// localStorage.setItem('age', '26');
+// localStorage.setItem('country', 'USA');
+
